e48c28a5775926fa6f50048d7c772f28f02ec5ce
unknown <cqxg@yandex.ru>
2020-10-09 21:05:22 +0300

    feat: add simple get user by id error handling

--- src/resources/users/user.memory.repository.js
+++ src/resources/users/user.memory.repository.js
@@ -2,7 +2,11 @@ const DB = require('../../common/inMemoryDb');
 
 const getAll = async () => DB.getAllUsers();
 
-const get = async id => DB.getUser(id);
+const get = async id => {
+  const user = DB.getUser(id);
+  if (!user) throw new Error(`The user with id: ${id} was not found`);
+  return user;
+};
 
 const create = async user => {
   return DB.createUser(user);
