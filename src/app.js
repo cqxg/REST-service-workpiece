b12534af4916086ccf995cbe31d86d5391489f90
unknown <cqxg@yandex.ru>
2020-10-19 23:04:35 +0300

    fix: logging call

--- src/app.js
+++ src/app.js
@@ -13,6 +13,8 @@ const swaggerDocument = YAML.load(path.join(__dirname, '../doc/api.yaml'));
 
 app.use(express.json());
 
+logging(app);
+
 app.use('/doc', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 app.use('/', (req, res, next) => {
@@ -26,7 +28,5 @@ app.use('/', (req, res, next) => {
 app.use('/users', userRouter);
 app.use('/boards', boardRouter);
 app.use('/boards', taskRouter);
-
-logging(app);
 
 module.exports = app;
